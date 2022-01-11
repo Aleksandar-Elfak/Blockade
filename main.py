c53a70a3b74fe9f91c45c41bbc4cd45cc69e3a10
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2022-01-11 20:45:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,48 +42,58 @@ def tryParse(par):
 
 def startGame():
 
-    # boardRow = tryParse(input("Input number of rows: "))
-    # if boardRow < 11 or boardRow > 22:
-    #     print("Invalid rows")
-    #     return False
-    # boardColumn = tryParse(input("Input number of columns: "))
-    # if boardColumn < 14 or boardColumn > 28:
-    #     print("Invalid columns")
-    #     return  False
+    boardRow = tryParse(input("Input number of rows: "))
+    if boardRow < 11 or boardRow > 22:
+        print("Invalid rows")
+        return False
+    boardColumn = tryParse(input("Input number of columns: "))
+    if boardColumn < 14 or boardColumn > 28:
+        print("Invalid columns")
+        return False
 
-    # X = input("Position of X: ")
-    # if not validParameters(X, marking, boardRow, boardColumn):
-    #     print("invalid position")
-    #     return False
-    # x = input("Position of x: ")
-    # if not validParameters(x, marking, boardRow, boardColumn):
-    #     print("invalid position")
-    #     return False
-    # O = input("Position of O: ")
-    # if not validParameters(O, marking, boardRow, boardColumn):
-    #     print("invalid position")
-    #     return False
-    # o = input("Position of o: ")
-    # if not validParameters(o, marking, boardRow, boardColumn):
-    #     print("invalid position")
-    #     return False
-    # walls = tryParse(input("Number of walls: "))
-    # if walls < 9 or walls > 18:
-    #     print("Invalid number")
-    #     return False
-    # player = input("Choose X or O: ")
-    # if not (player == "X" or player == "x"):
-    #     if not (player == "O" or player == "o"):
-    #         print("You must choose X or O")
-    #         return False
-    # ai = None
-    # if player == "X" or player == "x":
-    #     ai = "O"
-    # else:
-    #     ai = "X"
+    X = input("Position of X: ")
+    if not validParameters(X, marking, boardRow, boardColumn):
+        print("invalid position")
+        return False
+    x = input("Position of x: ")
+    if not validParameters(x, marking, boardRow, boardColumn):
+        print("invalid position")
+        return False
+    O = input("Position of O: ")
+    if not validParameters(O, marking, boardRow, boardColumn):
+        print("invalid position")
+        return False
+    o = input("Position of o: ")
+    if not validParameters(o, marking, boardRow, boardColumn):
+        print("invalid position")
+        return False
+    walls = tryParse(input("Number of walls: "))
+    if walls < 9 or walls > 18:
+        print("Invalid number")
+        return False
+    player = input("Choose X or O: ")
+    if not (player == "X" or player == "x"):
+        if not (player == "O" or player == "o"):
+            print("You must choose X or O")
+            return False
+    ai = None
+    if player == "X" or player == "x":
+        ai = "O"
+    else:
+        ai = "X"
 
-    # game = Game(boardRow, boardColumn, X.split(" "), x.split(" "), O.split(" "), o.split(" "), walls, player, ai)
-    game = Game(7, 10, ("2", "2"), ("2", "9"), ("6", "2"), ("6", "9"), 6, "x", "O")
+    game = Game(
+        boardRow,
+        boardColumn,
+        X.split(" "),
+        x.split(" "),
+        O.split(" "),
+        o.split(" "),
+        walls,
+        player,
+        ai,
+    )
+    # game = Game(7, 10, ("2", "2"), ("2", "9"), ("6", "2"), ("6", "9"), 6, "x", "O")
     game.play()
 
 
